ba9175353f8fd6c216273e5518bc8a1586567d2a
Yur1W <165611276+Yur1W@users.noreply.github.com>
2025-11-11 19:39:04 -0300

    Update Player Controller.cs

--- Assets/Scripts/Gameplay/Player Controller.cs
+++ Assets/Scripts/Gameplay/Player Controller.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerControllerIso : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    public float speed = 2f;
+    [SerializeField]
+    SpriteRenderer sprite;
+    Rigidbody2D rb;
+    Vector2 input;
+    Vector3 mousePos;
+    Vector3 lookDir;
 
-    // Update is called once per frame
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        sprite = GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
-        
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        input = Vector2.ClampMagnitude(input, 1f);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        ChangeDirection();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = input * speed;
+    }
+    void ChangeDirection()
+    {
+       Vector3 lookDir = mousePos - transform.position;
+        if (lookDir.x >= 0)
+        {
+            sprite.flipX = false;
+        }
+        else
+        {
+            sprite.flipX = true;
+        } 
     }
 }
+
