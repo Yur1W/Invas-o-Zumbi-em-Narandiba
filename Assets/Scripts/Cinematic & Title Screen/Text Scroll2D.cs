e8b7d64292eb31b290c524af0b1e797e604686b0
Yur1W <165611276+Yur1W@users.noreply.github.com>
2025-11-15 14:33:48 -0300

    updated

--- Assets/Scripts/Cinematic & Title Screen/Text Scroll2D.cs
+++ Assets/Scripts/Cinematic & Title Screen/Text Scroll2D.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TextScroll2D : MonoBehaviour
 {
@@ -26,5 +27,14 @@ public class TextScroll2D : MonoBehaviour
         Vector2 pos = rect.anchoredPosition;
         pos.y += scrollSpeed * Time.deltaTime;
         rect.anchoredPosition = pos;
+        if (pos.y > endY)
+        {
+            StartCoroutine(LoadNextSceneAfterDelay(1f));
+        }
+    }
+    IEnumerator LoadNextSceneAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
